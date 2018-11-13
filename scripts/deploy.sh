51796302e5801adff273ca6d68286b4faa028d82
Lin, Bernard <bernardlin@outlook.com>
2018-11-13 17:49:56 -0500

    revise deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-if [ -n 'engima-template-dapp' ];then 
-  rm -rf enigma-template-dapp;
-fi
-git clone --single-branch -b millionaires_problem_demo https://github.com/iLookaboutlodge/enigma-template-dapp.git
 cd enigma-template-dapp
 npm install
 darq-truffle migrate --reset --network development
