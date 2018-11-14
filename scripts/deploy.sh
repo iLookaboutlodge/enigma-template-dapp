6f2f9484212d882ff7d5e2fb0153c38fc245ce3d
Lin, Bernard <bernardlin@outlook.com>
2018-11-14 10:12:45 -0500

    udpate deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-cd enigma-template-dapp
+cd ~/enigma-template-dapp
 npm install
 darq-truffle migrate --reset --network development
-cd enigma-template-dapp/client 
+cd ~/enigma-template-dapp/client 
 npm install
 nohup npm run start &
 
